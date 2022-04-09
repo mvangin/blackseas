51d345dac36b4c17c869828d7b9b228251fb4074
Marcus <43646116+mvangin@users.noreply.github.com>
2022-04-08 20:16:54 -0400

    test api deploy

--- api/app.js
+++ api/app.js
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { res.send('Hello from Express!')
+app.get('/', (req, res) => { res.send('Hello from Express!')})
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
