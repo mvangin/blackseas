24cfb29593e3c02bfe99031214f6a9d5011190ca
Marcus <43646116+mvangin@users.noreply.github.com>
2022-04-08 20:08:06 -0400

    test

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cors = require("cors");
 
 
+
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -23,7 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => { res.send('Hello from Express!')
+
+//app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
