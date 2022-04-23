ea95099f405d8832a47fcd3ed10d88a944f6e2d1
Marcus <43646116+mvangin@users.noreply.github.com>
2022-04-22 21:56:54 -0400

    press section

--- client/src/components/Press/Press.js
+++ client/src/components/Press/Press.js
@@ -4,11 +4,116 @@ import classes from "./press.module.css"
 
 
 function Press() {
-
-
     return (
-        <div id="press" className={`${classes.pressSection}`}>
-            <h1 className="text-center" > Press </h1>
+        <div className={` mt-4 pt-4`}>
+            <h1 className={`${classes.pressHeader}`}> Selected Publications </h1>
+            <div className={`${classes.pressContainer}`}>
+                <div className={`${classes.pressSection}`}>
+                    <h2 className={`${classes.pressSectionHeader}`}> Major Regional and Thematic Reports</h2>
+                    <ul>
+                        <li>
+                            “Violence at Sea: How Terrorists, Insurgents, and Other Extremists Exploit the Maritime Domain."
+                            Published by Stable Seas, One Earth Future. 2020. Authored with Christopher Faulkner (University of Central Florida), Curtis Bell, Meghan Curran, Tyler Lycan, and Jay Benson (all Stable Seas).
+                        </li>
+                        <li>
+                            “Stable Seas: Bay of Bengal.” Published by Stable Seas, One Earth Future. 2020. Authored with Jay Benson, Curtis Bell, Lydelle Joubert, Maisie Pigeon, Asyura Salleh (all stable seas), Gayathri Iyer, (Observer Research Foundation), M J Rahman (World Fish), and M A Wahab (World Fish).
+                            Policy Brief Publications
+                        </li>
+                    </ul>
+                </div>
+                <div className={`${classes.pressSection}`}>
+                    <h2 className={`${classes.pressSectionHeader}`}> Policy Brief Publications </h2>
+                    <ul>
+                        <li>
+                            "Black Sea Synergy: A Holistic Approach to Maritime Security." Published by Europeum institute of European Policy. 2022. Author.
+                        </li>
+                        <li>
+                            “Addressing Maritime Security in Southeast Asia”, Published by Stable Seas, One Earth Future. 2021. Author.
+
+                        </li>
+                        <li>
+                            “BARMM Blue Economy: Policy Proposals For the Bangsamoro Autonomous Region in Muslim
+                            Mindanao.” Published by Stable Seas, One Earth future. 2020. Author.
+                        </li>
+                        <li>
+                            “Challenges and Solutions for Maritime Security in the Indian Ocean.” Published by Stable Seas. 2020.
+                            Author.
+                        </li>
+                        <li>
+                            “A Pathway to Policy Change: Improving Philippine Fisheries, Blue Economy, and Maritime Law Enforcement in the Sulu and Celebes Seas.” Stable Seas, One Earth Future. 2019. Author.
+                        </li>
+                    </ul>
+                </div>
+                <div className={`${classes.pressSection}`}>
+                    <h2 className={`${classes.pressSectionHeader}`}>Open Editorials </h2>
+                    <ul>
+                        <li>
+                            “Opioid Smuggling in the Black Sea.” Published by Eurasian Review. 2022. Author
+                        </li>
+
+                        <li>
+                            "The Hungarian Ukrainian Schism." Published by Real Clear Defense. 2022. Author.
+
+                        </li>
+                        <li>
+                            “Moving Forward: Japan’s Maritime Policy Trajectory in the Philippines.” Published by
+                            Analyzing War. 2021. Author.
+                        </li>
+                        <li>
+                            “Deconstructing Abu Sayyaf’s Resilience to Counter-Insurgency Operations”, The Diplomat
+                        </li>
+                        <li>
+                            “Countering Maritime Drug Trafficking in Bangladesh’s Coastal Waters.” Published by Stable Seas, One
+                            earth future. 2020. Author.
+                        </li>
+                        <li>
+                            “A South Pacific Island-Led Approach to Regional Maritime Security.“ Published by Center for International
+                            Maritime Security. 2020. Author.
+                        </li>
+                        <li>
+                            “Thailand’s Deep South Insurgencies: Exploiting the Maritime Domain.” Published by The Diplomat. 2020.
+                            Author.
+                        </li>
+                        <li>
+                            “Using Geospatial Data to Improve Maritime Domain Awareness in the Sulu and Celebes Seas.” Published
+                            by Center for International Maritime Security. 2020. Author.
+                        </li>
+                        <li>
+                            “A Collaborative Approach to Improving Maritime Security in the Bay of Bengal.” Published by Stable
+                            Seas, One Earth Future. 2020. Author.
+                        </li>
+                        <li>
+                            “Tamil Sea Tigers: Exploiting the Maritime Domain.” Published by Stable Sea, One Earth Future. Author.
+                        </li>
+                        <li>
+                            “Engaging With Local Stakeholders to Improve Maritime Security and Governance.” Published by Modern
+                            Diplomacy. 2020. Author.
+                        </li>
+                        <li>
+                            “Japan’s Contributions to Maritime Stability in the Bay of Bengal.” Published by East Asia Forum. 2020.
+                            Author.
+                        </li>
+                        <li>
+                            “How a Myanmar Coast Guard Will Contribute to Coastal Stability.” Published by The Diplomat. 2020.
+                            Author.
+                        </li>
+                        <li>
+                            “Community Engagement: Improving Maritime Domain Awareness in the Bay of Bengal.” Published by
+                            Stable Seas, One Earth Future. 2020 Author
+                        </li>
+                        <li>
+                            “The Future of Philippine Maritime Enforcement.” Published by ASEAN Post. 2019. Author.
+                        </li>
+                        <li>
+                            “Deterrence and Inclusivity in the South China Sea.” Published by Global Politics Magazine. 2019. Author.
+                        </li>
+                        <li>
+                            “An Advisory Capacity: The Wider Ramifications of Security Force Assistance Brigades in
+                            Afghanistan.” Published by Small Wars Journal. 2019. Author.
+                        </li>
+                    </ul>
+                </div>
+            </div >
         </div>
     )
 }
