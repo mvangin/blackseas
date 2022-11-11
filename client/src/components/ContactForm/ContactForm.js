178a92e259ae2cc9cebb0d5c86b14e1dfc10d340
Marcus van Ginkel <mvanginkel@mooveguru.com>
2022-11-11 13:10:41 -0600

    update contact form to have form validation

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -1,91 +1,124 @@
-import React, { useState } from 'react'
-import api from '../../api'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import classes from "./contactForm.module.css"
+import React, { useState } from 'react';
+import api from '../../api';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import classes from './contactForm.module.css';
 
 function ContactForm({ show = true, onHide }) {
-    const [email, setEmail] = useState("")
-    const [name, setName] = useState("");
-    const [content, setContent] = useState("");
-    const [messageSent, setMessageSent] = useState(false);
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const [content, setContent] = useState('');
+	const [messageSent, setMessageSent] = useState(false);
+	const [validationErrors, setValidationErrors] = useState('');
 
-    function handleClose() {
-        setEmail("")
-        setName("")
-        setContent("")
-        onHide();
-    }
+	function handleClose() {
+		onHide();
+		setEmail('');
+		setName('');
+		setContent('');
+	}
 
-    function handleSubmit(e) {
-        e.preventDefault();
+	function handleSubmit(e) {
+		e.preventDefault();
 
-        let payload = { email, name, content }
-        api.contactPost(payload).then(res => {
-            setMessageSent(true);
-        })
-    }
+		if (!email.length | !name.length) {
+			setValidationErrors('Name and Email are required.');
+			return;
+		}
 
-    return (
-        <div id="contact-form">
-            <Modal
-                onHide={handleClose}
-                show={show}
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title className={`${classes.modalTitle}`} >
-                    {!messageSent && "Contact Us"}
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {messageSent ?
-                        <div className={`${classes.messageSent}`}>
-                            Message Sent! We will get back to you soon.</div>
-                        :
-                        <form onSubmit={handleSubmit} className={`${classes.contactForm}`} >
-                            <label>
-                                <input
-                                    placeholder="Name"
-                                    className={`${classes.postInput}`}
-                                    value={name}
-                                    onChange={(e) => { setName(e.target.value); }} />
-                            </label>
-                            <label>
-                                <input
-                                    placeholder="Email"
-                                    className={`${classes.postInput}`}
-                                    value={email}
-                                    onChange={(e) => { setEmail(e.target.value); }} />
-                            </label>
-                            <label>
-                                <textarea
-                                    rows={5}
-                                    placeholder="Content"
-                                    className={`${classes.postInput}`}
-                                    value={content}
-                                    onChange={(e) => { setContent(e.target.value); }} />
-                            </label>
-                        </form>}
+		let payload = { email, name, content };
 
-                </Modal.Body>
-                <Modal.Footer
-                    className={`${classes.contactFooter}`}>
-                    {messageSent ? <Button
-                        variant="success"
-                        className={`${classes.sendButton}`}
-                        onClick={handleClose}>
-                        Close </Button> :
-                        <Button
-                            variant="success"
-                            className={`${classes.sendButton} `}
-                            onClick={handleSubmit}>
-                            Send Message</Button>}
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
+		try {
+			api.contactPost(payload).then((res) => {
+				setMessageSent(true);
+			});
+		} catch (error) {
+			console.debug(error);
+		}
+	}
+
+	return (
+		<div id="contact-form">
+			<Modal
+				onHide={handleClose}
+				show={show}
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title className={`${classes.modalTitle}`}>
+						{!messageSent && 'Contact Us'}
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{messageSent ? (
+						<div className={`${classes.messageSent}`}>
+							Message Sent! We will get back to you soon.
+						</div>
+					) : (
+						<form
+							onSubmit={handleSubmit}
+							className={`${classes.contactForm}`}
+						>
+							<label>
+								<input
+									required={true}
+									placeholder="Name"
+									className={`${classes.postInput}`}
+									value={name}
+									onChange={(e) => {
+										setName(e.target.value);
+									}}
+								/>
+							</label>
+							<label>
+								<input
+									required={true}
+									placeholder="Email"
+									className={`${classes.postInput}`}
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value);
+									}}
+								/>
+							</label>
+							<label>
+								<textarea
+									rows={5}
+									placeholder="Content"
+									className={`${classes.postInput}`}
+									value={content}
+									onChange={(e) => {
+										setContent(e.target.value);
+									}}
+								/>
+							</label>
+							{validationErrors && <div className="text-danger mt-2"> {validationErrors} </div>}
+						</form>
+					)}
+				</Modal.Body>
+				<Modal.Footer className={`${classes.contactFooter}`}>
+					{messageSent ? (
+						<Button
+							variant="success"
+							className={`${classes.sendButton}`}
+							onClick={handleClose}
+						>
+							Close
+						</Button>
+					) : (
+						<Button
+							variant="success"
+							className={`${classes.sendButton} `}
+							onClick={handleSubmit}
+						>
+							Send Message
+						</Button>
+					)}
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 }
 
-export default ContactForm
+export default ContactForm;
