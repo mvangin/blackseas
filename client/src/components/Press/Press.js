ec479177a2d37ac414a2c2327f6a5907f0d652fc
Marcus van Ginkel <mvanginkel@mooveguru.com>
2022-11-11 13:10:58 -0600

    update Press UI

--- client/src/components/Press/Press.js
+++ client/src/components/Press/Press.js
@@ -1,128 +1,131 @@
-import React from "react"
-import classes from "./press.module.css"
-
-
+import React, { useState, useEffect } from 'react';
+import pressList from '../../dataFiles/press';
+import classes from './press.module.css';
 
 function Press() {
-    return (
-        <div className={` mt-4 pt-4`} id="press">
-            <h1 className={`${classes.pressHeader}`}> Our Publications </h1>
-            <div className={`${classes.pressContainer}`}>
-                <div className={`${classes.pressSection}`}>
-                    <h2 className={`${classes.pressSectionHeader}`}> Major Regional and Thematic Reports</h2>
-                    <ul>
-                        <li>
-                            <span>“Violence at Sea: How Terrorists, Insurgents, and Other Extremists Exploit the Maritime Domain."</span><br/>
-                            Published by Stable Seas, One Earth Future. 2020. Authored with Christopher Faulkner (University of Central Florida), Curtis Bell, Meghan Curran, Tyler Lycan, and Jay Benson (all Stable Seas).
-                        </li>
-                        <li>
-                            <span>“Stable Seas: Bay of Bengal.”</span><br/> Published by Stable Seas, One Earth Future. 2020. Authored with Jay Benson, Curtis Bell, Lydelle Joubert, Maisie Pigeon, Asyura Salleh (all stable seas), Gayathri Iyer, (Observer Research Foundation), M J Rahman (World Fish), and M A Wahab (World Fish).
-                            Policy Brief Publications
-                        </li>
-                    </ul>
-                </div>
-                <div className={`${classes.pressSection}`}>
-                    <h2 className={`${classes.pressSectionHeader}`}> Policy Brief Publications </h2>
-                    <ul>
-                        <li>
-                            <span>"Black Sea Synergy: A Holistic Approach to Maritime Security." </span><br/> Published by Europeum institute of European Policy. 2022. Author.
-                        </li>
-                        <li>
-                            <span>“Addressing Maritime Security in Southeast Asia”, </span><br/> Published by Stable Seas, One Earth Future. 2021. Author.
+	const [regionalThematic, setRegionalThematic] = useState([]);
+	const [policyBrief, setPolicyBrief] = useState([]);
+	const [openEditorial, setOpenEditorial] = useState([]);
+	const [showPress, setShowPress] = useState([]);
 
-                        </li>
-                        <li>
-                            <span>
-                                “BARMM Blue Economy: Policy Proposals For the Bangsamoro Autonomous Region in Muslim
-                                Mindanao.”
-                            </span><br/>  Published by Stable Seas, One Earth future. 2020. Author.
-                        </li>
-                        <li>
-                            <span>“Challenges and Solutions for Maritime Security in the Indian Ocean.”</span><br/>  Published by Stable Seas. 2020.
-                            Author.
-                        </li>
-                        <li>
-                            <span>“A Pathway to Policy Change: Improving Philippine Fisheries, Blue Economy, and Maritime Law Enforcement in the Sulu and Celebes Seas.”</span> <br/>  Stable Seas, One Earth Future. 2019. Author.
-                        </li>
-                    </ul>
-                </div>
+	useEffect(() => {
+		const regionalThematicList = filterPressListByCategory(
+			pressList,
+			'major_regional_thematic_report'
+		);
+		setRegionalThematic(regionalThematicList);
 
-                <div className={`${classes.pressSection}`}>
-                    <h2 className={`${classes.pressSectionHeader}`}>Open Editorials </h2>
-                    <div>
-                        <ul>
-                            <li>
-                                <span>“Opioid Smuggling in the Black Sea.”</span> <br/> Published by Eurasian Review. 2022. Author
-                            </li>
+		const policyBriefList = filterPressListByCategory(
+			pressList,
+			'policy_brief_publication'
+		);
+		setPolicyBrief(policyBriefList);
 
-                            <li>
-                                <span>"The Hungarian Ukrainian Schism." </span><br/> Published by Real Clear Defense. 2022. Author.
+		const openEditorialList = filterPressListByCategory(
+			pressList,
+			'open_editorial'
+		);
+		setOpenEditorial(openEditorialList);
+	}, []);
 
-                            </li>
-                            <li>
-                                <span>“Moving Forward: Japan’s Maritime Policy Trajectory in the Philippines.”</span><br/>  Published by
-                                Analyzing War. 2021. Author.
-                            </li>
-                            <li>
-                                <span>“Deconstructing Abu Sayyaf’s Resilience to Counter-Insurgency Operations”,</span> <br/>  The Diplomat
-                            </li>
-                            <li>
-                                <span>“Countering Maritime Drug Trafficking in Bangladesh’s Coastal Waters.” </span><br/>  Published by Stable Seas, One
-                                earth future. 2020. Author.
-                            </li>
-                            <li>
-                                <span>“A South Pacific Island-Led Approach to Regional Maritime Security.“</span><br/>  Published by Center for International
-                                Maritime Security. 2020. Author.
-                            </li>
-                            <li>
-                                <span>“Thailand’s Deep South Insurgencies: Exploiting the Maritime Domain.”</span><br/>  Published by The Diplomat. 2020.
-                                Author.
-                            </li>
-                            <li>
-                                <span>“Using Geospatial Data to Improve Maritime Domain Awareness in the Sulu and Celebes Seas.”</span> <br/> Published
-                                by Center for International Maritime Security. 2020. Author.
-                            </li>
-                            <li>
-                                <span>“A Collaborative Approach to Improving Maritime Security in the Bay of Bengal.” </span><br/> Published by Stable
-                                Seas, One Earth Future. 2020. Author.
-                            </li>
-                            <li>
-                                <span>“Tamil Sea Tigers: Exploiting the Maritime Domain.”</span> <br/> Published by Stable Sea, One Earth Future. Author.
-                            </li>
-                            <li>
-                                <span>“Engaging With Local Stakeholders to Improve Maritime Security and Governance.”</span> <br/> Published by Modern
-                                Diplomacy. 2020. Author.
-                            </li>
-                            <li>
-                                <span>“Japan’s Contributions to Maritime Stability in the Bay of Bengal.”</span> <br/> Published by East Asia Forum. 2020.
-                                Author.
-                            </li>
-                            <li>
-                                <span>“How a Myanmar Coast Guard Will Contribute to Coastal Stability.”</span> <br/> Published by The Diplomat. 2020.
-                                Author.
-                            </li>
-                            <li>
-                                <span>“Community Engagement: Improving Maritime Domain Awareness in the Bay of Bengal.”</span> <br/> Published by
-                                Stable Seas, One Earth Future. 2020 Author
-                            </li>
-                            <li>
-                                <span>“The Future of Philippine Maritime Enforcement.”</span> <br/> Published by ASEAN Post. 2019. Author.
-                            </li>
-                            <li>
-                                <span>“Deterrence and Inclusivity in the South China Sea.”</span> <br/> Published by Global Politics Magazine. 2019. Author.
-                            </li>
-                            <li>
-                                <span>
-                                    “An Advisory Capacity: The Wider Ramifications of Security Force Assistance Brigades in
-                                    Afghanistan.”
-                                </span> <br/> Published by Small Wars Journal. 2019. Author.
-                            </li>
-                        </ul>
-                    </div>
-                </div >
-            </div>
-        </div>
-    )
+	function renderPressList(pressList) {
+		return pressList.map((pressItem) => (
+			<li key={pressItem.title}>
+				<span>{pressItem.title} </span>
+				<br /> {pressItem.publisher} {pressItem.year} {pressItem.author}
+			</li>
+		));
+	}
+
+	function filterPressListByCategory(pressList, category) {
+		return pressList.filter((pressItem) => pressItem.category === category);
+	}
+
+	function handleShowPress(category) {
+		if (showPress.includes(category)) {
+			setShowPress((prevState) =>
+				prevState.filter((item) => item !== category)
+			);
+			return;
+		}
+
+		setShowPress((prevState) => [...prevState, category]);
+	}
+
+	return (
+		<div className={` mt-4 pt-4`} id="press">
+			<h1 className={`${classes.pressHeader}`}> Our Publications </h1>
+			<div className={`${classes.pressContainer}`}>
+				<div className={`${classes.pressSection}`}>
+					<button
+						className={`${classes.pressExpandButton}`}
+						onClick={() => handleShowPress('regionalThematic')}
+					>
+						<h2 className={`${classes.pressSectionHeader}`}>
+							Major Regional and Thematic Reports
+							<div
+								className={`${classes.downArrow}  ${
+									showPress.includes('regionalThematic') &&
+									classes.pressedExpandButton
+								}`}
+							>
+								»
+							</div>
+						</h2>
+					</button>
+					{showPress.includes('regionalThematic') && (
+						<ul>{renderPressList(regionalThematic)}</ul>
+					)}
+				</div>
+
+				<div className={`${classes.pressSection}`}>
+					<button
+						className={`${classes.pressExpandButton}`}
+						onClick={() => handleShowPress('policyBrief')}
+					>
+						<h2 className={`${classes.pressSectionHeader}`}>
+							Policy Brief Publication
+							<div
+								className={`${classes.downArrow} ${
+									showPress.includes('policyBrief') &&
+									classes.pressedExpandButton
+								} `}
+							>
+								»
+							</div>
+						</h2>
+					</button>
+					{showPress.includes('policyBrief') && (
+						<ul>{renderPressList(policyBrief)}</ul>
+					)}
+				</div>
+
+				<div className={`${classes.pressSection}`}>
+					<button
+						className={`${classes.pressExpandButton}`}
+						onClick={() => handleShowPress('openEditorial')}
+					>
+						<h2 className={`${classes.pressSectionHeader} `}>
+							Open Editorials
+							<div
+								className={`${classes.downArrow} ${
+									showPress.includes('openEditorial') &&
+									classes.pressedExpandButton
+								} `}
+							>
+								»
+							</div>
+						</h2>
+					</button>
+					<div>
+						{showPress.includes('openEditorial') && (
+							<ul>{renderPressList(openEditorial)}</ul>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Press
+export default Press;
